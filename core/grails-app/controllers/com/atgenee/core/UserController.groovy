bab097f0c4e7a9b5f7fec2ba07d6adc7e73c2131
Genee <342418262@qq.com>
2018-11-29 16:30:39 +0800

    upgrade

--- core/grails-app/controllers/com/atgenee/core/UserController.groovy
+++ core/grails-app/controllers/com/atgenee/core/UserController.groovy
@@ -1,9 +1,12 @@
 package com.atgenee.core
 
+import grails.converters.JSON
 import groovy.util.logging.Slf4j
 
 @Slf4j
 class UserController {
+
+//    static allowedMethods = [save: "POST"]
 
     def userService
 
@@ -11,5 +14,30 @@ class UserController {
         def result = userService.hello()
         log.error("hello,the world! {}",result)
         render result
+    }
+
+
+    def save() {
+        println params
+        println userService
+        def info = [code:0, msg:"保存失败"]
+        def user = userService.save(params)
+        if(user) {
+            info.code = 1
+            info.msg = "保存成功"
+            info.user = user
+        }
+        render info as JSON
+    }
+
+    def update() {
+        def info = [code:0, msg:"保存失败"]
+        def user = userService.update(params)
+        if(user) {
+            info.code = 1
+            info.msg = "保存成功"
+            info.user = user
+        }
+        render info as JSON
     }
 }
